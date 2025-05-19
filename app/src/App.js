6b42e054a5420b1761446cf3ca0b164a321898e0
go0leum <mijin575@gmail.com>
2025-05-19 16:40:45 +0900

    Update:RecentFile

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import FileProvider from './context/FileContext.js';
+
 import RecentFile from './pages/RecentFile';
 import ReviewTable from './pages/ReviewTable';
 import DocumentOCR from './pages/DocumentOCR.js';
@@ -8,11 +10,13 @@ import DocumentOCR from './pages/DocumentOCR.js';
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/recentFiles" element={<RecentFile />} />
-        <Route path="/reviewTable" element={<ReviewTable />} />
-        <Route path="/documentOCR" element={<DocumentOCR />} />
-      </Routes>
+      <FileProvider>
+        <Routes>
+          <Route path="/recentFile" element={<RecentFile />} />
+          <Route path="/reviewTable/:file_name" element={<ReviewTable />} />
+          <Route path="/documentOCR/:file_name" element={<DocumentOCR />} />
+        </Routes>
+      </FileProvider>
     </Router>
   );
 }
